0778ae220b85d720f566623c8d7d0a10a79e06e8
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-04 21:58:01 +0900

    Add UI of LoginForm component

---
+++ components/LoginForm.tsx
@@ -0,0 +1,52 @@
+import { Button, Form, Input } from 'antd';
+import Link from 'next/link';
+import styled from 'styled-components';
+import useForm from '../hooks/useForm';
+
+const LoginForm = () => {
+  const { state, handleOnChange, handleOnSubmit } = useForm();
+
+  const { userId, password } = state;
+  return (
+    <Form>
+      <div>
+        <label htmlFor="userId">아이디</label>
+        <Input
+          id="userId"
+          type="text"
+          name="userId"
+          value={userId}
+          onChange={handleOnChange}
+          required
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">비밀번호</label>
+        <Input
+          id="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleOnChange}
+          required
+        />
+      </div>
+
+      <ButtonContainer>
+        <Button type="primary" htmlType="submit" onClick={handleOnSubmit}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </ButtonContainer>
+    </Form>
+  );
+};
+
+export default LoginForm;
+
+const ButtonContainer = styled.div`
+  margin-top: 10px;
+`;
