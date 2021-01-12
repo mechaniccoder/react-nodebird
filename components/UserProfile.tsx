646b34460443d8ee76e311b8d9c9ed2227ea3dff
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-12 18:19:41 +0900

    Add UserProfile component using card in antd

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,3 +1,36 @@
-const UserProfile = () => {};
+import { FC, useCallback } from 'react';
+import { Card, Avatar, Button } from 'antd';
+
+interface Props {
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const UserProfile: FC<Props> = ({ setIsLoggedIn }) => {
+  const onLogout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, [setIsLoggedIn]);
+
+  return (
+    <Card
+      actions={[
+        <div key="twit">
+          짹짹
+          <br />0
+        </div>,
+        <div key="followings">
+          Following
+          <br />0
+        </div>,
+        <div key="followers">
+          Follower
+          <br />0
+        </div>,
+      ]}
+    >
+      <Card.Meta avatar={<Avatar>MC</Avatar>} title="Mechaniccoder" />
+      <Button onClick={onLogout}>로그아웃</Button>
+    </Card>
+  );
+};
 
 export default UserProfile;
