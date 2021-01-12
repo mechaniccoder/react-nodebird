9a8cd7ff733b4a223c4e68cc5e7293845a879c1f
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-12 23:23:59 +0900

    Add component to show follwers and followings

---
+++ components/Profile/FollowList.tsx
@@ -0,0 +1,45 @@
+import { Button, Card, List } from 'antd';
+import { FC } from 'react';
+import styled from 'styled-components';
+import { StopOutlined } from '@ant-design/icons';
+
+interface DataItem {
+  nickname: string;
+}
+
+interface Props {
+  title: string;
+  data: DataItem[];
+}
+
+const FollowList: FC<Props> = ({ title, data }) => {
+  return (
+    <ListWrapper
+      size="small"
+      grid={{ gutter: 4, column: 3 }}
+      header={<div>{title}</div>}
+      bordered
+      loadMore={
+        <div
+          style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}
+        >
+          <Button>더보기</Button>
+        </div>
+      }
+      dataSource={data}
+      renderItem={(item: DataItem) => (
+        <List.Item>
+          <Card actions={[<StopOutlined />]}>
+            <Card.Meta description={item.nickname} />
+          </Card>
+        </List.Item>
+      )}
+    />
+  );
+};
+
+export default FollowList;
+
+const ListWrapper = styled(List)`
+  margin-top: 15px;
+`;
