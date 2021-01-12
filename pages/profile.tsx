02848e57b6246889fbf87e74fdad40d2001a7f6d
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-12 23:24:48 +0900

    Update profile page UI using dummy data

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,14 +1,31 @@
 import Layout from '../components/Layout';
 
 import Head from 'next/head';
+import NicknameEditForm from '@components/Profile/NicknameEditForm';
+import FollowList from '@components/Profile/FollowList';
 
 const Profile = () => {
+  const follwings = [
+    { nickname: '여명' },
+    { nickname: '승환' },
+    { nickname: '성하' },
+  ];
+  const follwers = [
+    { nickname: '준영' },
+    { nickname: '상규' },
+    { nickname: '헬로' },
+  ];
+
   return (
     <>
       <Head>
         <title>프로필 | NodeBird</title>
       </Head>
-      <Layout>프로필</Layout>
+      <Layout>
+        <NicknameEditForm />
+        <FollowList title="Followings" data={follwings} />
+        <FollowList title="Follwers" data={follwers} />
+      </Layout>
     </>
   );
 };
