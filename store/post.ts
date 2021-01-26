eddb6e3be9ed3d64f6ed4ccb5ee42400b9d076db
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-26 23:35:13 +0900

    Add dummy data

--- store/post.ts
+++ store/post.ts
@@ -1,11 +1,56 @@
+interface MainPost {
+  id: number;
+  User: { id: number; nickname: string };
+  content: string;
+  Images: { src: string }[];
+}
+
 interface InitialPost {
-  text: string;
+  mainPosts: MainPost[];
+  imagePaths: string[];
+  postAdded: boolean;
 }
 
 const initialPost: InitialPost = {
-  text: '',
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: '승환',
+      },
+      content: '첫 게시물 #nextJs #docker',
+      Images: [
+        {
+          src:
+            'https://images.unsplash.com/photo-1611545815820-c1a58530d8d6?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+        },
+        {
+          src:
+            'https://images.unsplash.com/photo-1611651336487-802fe164d3e5?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
 };
 
-export default function post(state = initialPost, action: any) {
+const dummyPost: MainPost = {};
+
+export const addPost = {
+  type: 'ADD_POST',
+};
+
+export default function post(state = initialPost, action: any): InitialPost {
+  switch (action.type) {
+    case 'ADD_POST':
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+      };
+    default:
+      return state;
+  }
   return state;
 }
