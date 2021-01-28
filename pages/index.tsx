1e99a2fff0688e1beec3f4d1bb176a399e6cfc5a
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-29 00:15:09 +0900

    Add component relative post into home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
+import PostCard from '@components/Post/PostCard';
+import PostForm from '@components/Post/PostForm';
+import { useSelector } from 'react-redux';
+import { rootState } from 'store/reducer';
 import Layout from '../components/Layout';
 
 export default function Home() {
-  return <Layout> </Layout>;
+  const { isLogIn } = useSelector((state: rootState) => state.user);
+  const { mainPosts } = useSelector((state: rootState) => state.post);
+
+  return (
+    <Layout>
+      {isLogIn && <PostForm />}
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </Layout>
+  );
 }
