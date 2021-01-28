f50b28b2cb7fe70cb3e3414e258c908930b29ca1
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-29 01:13:06 +0900

    Add image interface

--- type/index.ts
+++ type/index.ts
@@ -2,5 +2,9 @@ export interface MainPost {
   id: number;
   User: { id: number; nickname: string };
   content: string;
-  Images: { src: string }[];
+  Images: Image[];
+}
+
+export interface Image {
+  src: string;
 }
