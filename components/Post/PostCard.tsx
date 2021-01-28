c17dec63765ba75c8f9bdf94c89825d2c04cd7f8
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-29 01:13:27 +0900

    Update PostCard UI and Add state

--- components/Post/PostCard.tsx
+++ components/Post/PostCard.tsx
@@ -1,12 +1,78 @@
-import { FC } from 'react';
+import {
+  EllipsisOutlined,
+  HeartOutlined,
+  HeartTwoTone,
+  MessageOutlined,
+  RetweetOutlined,
+} from '@ant-design/icons';
+import { Avatar, Button, Card, Popover } from 'antd';
+import { FC, useCallback, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { rootState } from 'store/reducer';
 import { MainPost } from 'type';
+import PostImages from './PostImages';
 
 interface Props {
   post: MainPost;
 }
 
 const PostCard: FC<Props> = ({ post }) => {
-  return <div>PostCard</div>;
+  const [liked, setLiked] = useState(false);
+  const [commentFormVisible, setCommentFormVisible] = useState(false);
+  const id = useSelector((state: rootState) => state.user.me?.id);
+
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev);
+  }, []);
+
+  const onToggleCommentForm = useCallback(() => {
+    setCommentFormVisible((prev) => !prev);
+  }, []);
+
+  return (
+    <div>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          liked ? (
+            <HeartTwoTone
+              key="heart"
+              twoToneColor="#eb2f96"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+          <MessageOutlined key="message" onClick={onToggleCommentForm} />,
+          <Popover
+            key="more"
+            content={
+              <Button.Group>
+                {id && post.User.id === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button danger>삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={post.content}
+        />
+      </Card>
+      {commentFormVisible && <div>comment form</div>}
+    </div>
+  );
 };
 
 export default PostCard;
