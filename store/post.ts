ceb8bf714fb36dd3ecd7d9f2aa1cf735756d18ea
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-29 01:12:37 +0900

    Add dummy data

--- store/post.ts
+++ store/post.ts
@@ -11,7 +11,7 @@ const initialPost: InitialPost = {
     {
       id: 1,
       User: {
-        id: 1,
+        id: 4,
         nickname: '승환',
       },
       content: '첫 게시물 #nextJs #docker',
@@ -31,7 +31,24 @@ const initialPost: InitialPost = {
   postAdded: false,
 };
 
-const dummyPost: MainPost = {};
+const dummyPost: MainPost = {
+  id: 1,
+  User: {
+    id: 3,
+    nickname: '상규',
+  },
+  content: '첫 게시물 #nextJs #docker',
+  Images: [
+    {
+      src:
+        'https://images.unsplash.com/photo-1611545815820-c1a58530d8d6?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      src:
+        'https://images.unsplash.com/photo-1611651336487-802fe164d3e5?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+  ],
+};
 
 const ADD_POST = 'post/add_post';
 
