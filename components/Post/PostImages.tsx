4bce4b5340b0f1a63383fe44dd2b060ad007ef09
mechanicoder <58871413+mechaniccoder@users.noreply.github.com>
2021-01-29 01:13:37 +0900

    Add PostImage component

---
+++ components/Post/PostImages.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import { Image } from 'type';
+
+interface Props {
+  images: Image[];
+}
+const PostImages: FC<Props> = ({ images }) => {
+  return <div>image</div>;
+};
+
+export default PostImages;
