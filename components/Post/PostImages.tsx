95b33d5fc26af64fbffc38f30622b4bd8cee01c1
mechaniccoder <yuseunghwan94@gmail.com>
2021-02-06 21:14:34 +0900

    Add PostImages component that is rendered differently by images length

--- components/Post/PostImages.tsx
+++ components/Post/PostImages.tsx
@@ -1,11 +1,74 @@
-import { FC } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import { FC, useCallback, useState } from 'react';
 import { Image } from 'type';
 
 interface Props {
   images: Image[];
 }
 const PostImages: FC<Props> = ({ images }) => {
-  return <div>image</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState<boolean>();
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom((prevShowImagesZoom) => !prevShowImagesZoom);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <img
+        style={{ maxHeight: '60vh', objectFit: 'contain' }}
+        src={images[0].src}
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+    );
+  }
+
+  if (images.length === 3) {
+    return (
+      <>
+        <img
+          style={{ width: '50%', display: 'inline-block' }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          style={{
+            width: '50%',
+            display: 'inline-block',
+            marginBottom: 'auto',
+            verticalAlign: 'top',
+          }}
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <img
+        style={{ width: '50%', display: 'inline-block' }}
+        src={images[0].src}
+        alt={images[0].src}
+        onClick={onZoom}
+      />
+      <div
+        style={{
+          display: 'inline-block',
+          width: '50%',
+          textAlign: 'center',
+        }}
+        onClick={onZoom}
+      >
+        <PlusOutlined />
+        <br />
+        {`${images.length - 1}개의 사진 더보기`}
+      </div>
+    </>
+  );
 };
 
 export default PostImages;
